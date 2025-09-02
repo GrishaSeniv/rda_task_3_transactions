958f23d0228cdae832f6602e1ef8323fb3a2f16b
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2025-09-02 20:37:18 +0300

    Refactored after review

--- task.sql
+++ task.sql
@@ -2,14 +2,23 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders () Values ();
 
 -- Start the transaction 
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction
-INSERT INTO Orders (CustomerID, Date) VALUES (1, '2025-09-02');
+UPDATE Orders
+SET CustomerID = 1,
+Date = '2025-09-02'
+WHERE ID = LAST_INSERT_ID();
 
 INSERT INTO OrderItems(OrderID, ProductID, Count) values (LAST_INSERT_ID(), 1, 1);
+
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;
+
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
